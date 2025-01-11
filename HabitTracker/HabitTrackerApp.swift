17a98cbdcce517f55fb7576b54ce55e60e3468db
Mohanad-Ramadan <mohanadramadan.m1@gmail.com>
2025-01-12 01:20:49 +0200

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Mohanad Ramdan on 12/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
